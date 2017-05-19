ecb67e083c46fa4cf65ca8aa4b347b7b53b29125
fajar <fajarpm786@gmail.com>
2017-05-19 16:05:31 +0530

    19-may updated pop up with timer added icon in splash screen

--- riderapp/src/main/java/com/masaga/goyorider/forms/dashboard.java
+++ riderapp/src/main/java/com/masaga/goyorider/forms/dashboard.java
@@ -2,28 +2,38 @@ package com.masaga.goyorider.forms;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.PopupWindow;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.hanks.htextview.HTextView;
 import com.masaga.goyorider.R;
 
+import java.util.concurrent.TimeUnit;
 import java.util.zip.Inflater;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static android.R.id.text1;
+
 public class dashboard extends AppCompatActivity {
-    
+
     @BindView(R.id.Pending_Order)
     FrameLayout Pending_Order;
     @BindView(R.id.Complated_Orders)
@@ -36,8 +46,12 @@ public class dashboard extends AppCompatActivity {
     FrameLayout Notifications;
     @BindView(R.id.All_Order)
     FrameLayout All_Order;
-    private int count=0;
+
     private PopupWindow OrderPopup;
+    private Button Btn_Accept, Btn_Reject;
+    private TextView Deliver_at_Text;
+    private TextView PopUp_CountText;
+    final Popup_Counter CountTimer = new Popup_Counter(180000,1000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,20 +63,54 @@ public class dashboard extends AppCompatActivity {
         actionBar.setCustomView(R.layout.rider_online_switch);
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_CUSTOM);
 
-        if(count==3){
-            initiatePopupWindow();
-        }
-
-
-    }
+}
 
     private void initiatePopupWindow() {
         try {
-            LayoutInflater layoutInflater=(LayoutInflater) dashboard.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View layout= View.inflate(R.id.Popup)
+            String role;
+            role="db";
+            LayoutInflater inflater=(LayoutInflater) dashboard.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.popup_orderummery,
+                    (ViewGroup) findViewById(R.id.PopUp));
+            OrderPopup= new PopupWindow(layout,850,700,true);
+            OrderPopup.showAtLocation(layout, Gravity.CENTER,0,0);
+            OrderPopup.setOutsideTouchable(true);
+            OrderPopup.setFocusable(true);
+            CountTimer.start();
+
+            Btn_Accept=(Button)layout.findViewById(R.id.Accept_order);
+            Btn_Reject=(Button)layout.findViewById(R.id.Reject_Order);
+            PopUp_CountText=(TextView)layout.findViewById(R.id.popup_counter);
+            Deliver_at_Text=(TextView)layout.findViewById(R.id.Deliver_at);
+
+            //If User role = "TL" or "Admin" Show address else don't show address
+            if(role.toLowerCase()=="db")
+                Deliver_at_Text.setText("xxx");
+            else
+                Deliver_at_Text.setText("Actual First Delivery Location");
+
+
+            Btn_Accept.setOnClickListener(accept_order_click);
+            Btn_Reject.setOnClickListener(reject_order_click);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
 
+
     }
+    private View.OnClickListener accept_order_click= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            OrderPopup.dismiss();
+        }
+    };
+    private View.OnClickListener reject_order_click= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            OrderPopup.dismiss();
+        }
+    };
 
     @OnClick(R.id.Pending_Order)
     void click(){
@@ -76,9 +124,9 @@ public class dashboard extends AppCompatActivity {
     }
     @OnClick(R.id.Complated_Orders)
     void click3(){
-        count=count+1;
-        Intent intent=new Intent(this,complated_order.class);
-        startActivity(intent);
+        initiatePopupWindow();
+        //Intent intent=new Intent(this,complated_order.class);
+        //startActivity(intent);
     }
     @OnClick(R.id.Rejected_Orders)
     void click4(){
@@ -94,5 +142,23 @@ public class dashboard extends AppCompatActivity {
     void click6(){
         Intent intent=new Intent(this,notification.class);
         startActivity(intent);
+    }
+    public class Popup_Counter extends CountDownTimer{
+
+        public Popup_Counter(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onFinish() {
+            OrderPopup.dismiss();
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+           PopUp_CountText.setText(""+String.format("%02d:%02d",
+                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+        }
     }
 }
